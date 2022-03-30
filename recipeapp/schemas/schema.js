689f404d47fa72daf53d43feb1ff4306b232940f
April <aprilnicole013@gmail.com>
2022-03-30 10:57:59 -0400

    Schemas created

--- recipeapp/schemas/schema.js
+++ recipeapp/schemas/schema.js
@@ -5,6 +5,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import chef from "./chef"
+import ingredient from './ingredient'
+import recipe from './recipe'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +16,8 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    chef
+    chef,
+    ingredient,
+    recipe
   ]),
 })
