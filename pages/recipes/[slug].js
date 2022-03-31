1e77a25fb64252eb68d8c2af3fb36d83bb7a53c8
April <aprilnicole013@gmail.com>
2022-03-31 10:33:17 -0400

    Fetch recipe paths and props

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -10,12 +10,7 @@ const recipeQuery = `*[_type== "recipe" && slug.current == $slug[0]]{
     _id,
     name,
     slug,
-    mainImage{
-        asset->{
-            _id,
-            url
-        }
-    },
+    mainImage,
     ingredient[]{
         unit,
         wholeNumber,
@@ -31,10 +26,29 @@ export default function OneRecipe(){
 
 }
 
+//pulls path from the content, data structure is created 
+//knwing the address of every house in neighborhhood. paths to all houses
 export async function getStaticPaths(){
-
+    const paths = await sanityClient.fetch(
+        `*[_type == "recipe" && defined(slug.current)]{
+            "params": {
+                "slug": slug.current
+            }
+        }`
+    )
+    return {
+        paths,
+        //always use true fallback so all paths are requested. if set to false, will result in a 404 error
+        fallback: true,
+    }
 }
 
-export async function getStaticProps(){
+//passed groq query into statc props. 
+//knowing the people in each house of the neighborhood and what they ate for breakfast. data and content within each house
+//groq params are passed as 2nd argument in function. matches slug query in recipeQuery
+export async function getStaticProps({ params }){
+    const { slug } = params;
+    const recipe = await sanityClient.fetch(recipeQuery, {slug})
 
+    return { props: { data: { recipe } } }
 }
